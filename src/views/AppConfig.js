eb9f337be85ae7e840dd31f29f50a501c315e64e
devsdisrupt <devs.disrupt@gmail.com>
2025-06-20 21:02:47 +0500

    Updated Push

---
+++ src/views/AppConfig.js
@@ -0,0 +1,7 @@
+export const appCred = {
+    buildVersion: "1.0",
+    // appUrl: "http://localhost:5000/",
+    appUrl: "https://dscribeaiapi-75705909274.europe-west1.run.app/api",
+    //appUrl: "https://url/api",
+    appServer: "SERVERNAME"
+  };
