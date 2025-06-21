d96fbb805c6af578b9beb5c9c3d9d8f4e11fb3ac
devsdisrupt <devs.disrupt@gmail.com>
2025-06-21 09:41:13 +0500

    KEY ADDED IN APPCONFIG

--- src/views/AppConfig.js
+++ src/views/AppConfig.js
@@ -4,4 +4,5 @@ export const appCred = {
     appUrl: "https://dscribeaiapi-75705909274.europe-west1.run.app/api",
     //appUrl: "https://url/api",
     appServer: "SERVERNAME"
+    key: "U2FsdGVkX1/komj2ZflR+PtAATCK9XVg7ku4BA9MmpKeRviFl4/dSjPtMt/EAfvytqpfD2LRcs/TRFEnrMtLgTsDE7AhTzolgU49sz/4j8lfivt/GgE/8Fhn0sUsP3rSnu9F3vU400HDgHOBpf/WtrFeWnSYjmKoKMLeh2JX+YzwRrzTYO7QzLFNtTTNfeFEGjgb5sPpM4if/mnHrPiu7IBEm67JnICYzViSqfW02NQGLR99OLSnbbj4ILgF5S0z"
   };
