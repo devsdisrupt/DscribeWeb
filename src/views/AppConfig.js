f3b91a6af206fded8555ed963254eabcbd3765a1
devsdisrupt <devs.disrupt@gmail.com>
2025-06-21 09:48:23 +0500

    ERROR FIXED

--- src/views/AppConfig.js
+++ src/views/AppConfig.js
@@ -3,6 +3,6 @@ export const appCred = {
     // appUrl: "http://localhost:5000/",
     appUrl: "https://dscribeaiapi-75705909274.europe-west1.run.app/api",
     //appUrl: "https://url/api",
-    appServer: "SERVERNAME"
+    appServer: "SERVERNAME",
     key: "U2FsdGVkX1/komj2ZflR+PtAATCK9XVg7ku4BA9MmpKeRviFl4/dSjPtMt/EAfvytqpfD2LRcs/TRFEnrMtLgTsDE7AhTzolgU49sz/4j8lfivt/GgE/8Fhn0sUsP3rSnu9F3vU400HDgHOBpf/WtrFeWnSYjmKoKMLeh2JX+YzwRrzTYO7QzLFNtTTNfeFEGjgb5sPpM4if/mnHrPiu7IBEm67JnICYzViSqfW02NQGLR99OLSnbbj4ILgF5S0z"
   };
